d661ff3df6124c9133102dc374f24ceb50dfc246
Max Cherepitsa <max.cherepitsa@gmail.com>
2016-03-14 12:24:32 +0700

    Add Ex. 1.6
    Тут DrRacket внезапно обработал код, который по логике аппликативного
    порядка должен был бы не сработать и несколько сбил меня с толку.

---
+++ 1_6.scm
@@ -0,0 +1,29 @@
+#lang racket
+
+; Тут трюк в том, что по идее из-за аппликативного порядка вычислений должно было бы зациклиться выполнение этого кода,
+; так как интерпретатор начал бы разворачивать процедуру, натыкался бы на рекурсивный кусок с sqrt-iter и зацикливался бы на нем.
+; Но ВНЕЗАПНО DrRacket почему-то скушал этот код и не подавился. Выдал правильный ответ.
+; MIT-Scheme интерпретатор Edwin, говорят, циклится как и задумано.
+
+(define (new-if predicate then-clause else-clause) (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (square x) (* x x))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+
+(sqrt 16)
